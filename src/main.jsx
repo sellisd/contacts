9d62638198adcc7e9dc58ff38d9f74556155b7cb
Diamantis Sellis <diamantis.sellis@cosmotech.com>
2024-03-22 16:18:27 +0100

    fix: completed

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Root, {loader as rootLoader, action as rootAction} from "./routes/root";
-import Contact, {loader as contactLoader, action as contactAction} from "./routes/contact";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/contact";
 import ErrorPage from "./error-page";
-import EditContact, {action as editAction} from "./routes/edit"
-import {action as destroyAction} from "./routes/destroy";
+import EditContact, { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
 import Index from "./routes/index";
 const router = createBrowserRouter([
   {
@@ -19,22 +25,33 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
-        path: "contacts/:contactId",
-        loader: contactLoader,
-        element: <Contact />,
-        action: contactAction,
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId",
+            loader: contactLoader,
+            element: <Contact />,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+          },
+        ],
       },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: destroyAction,
-      },
-      {index:true, element: <Index />}
     ],
   },
 ]);
